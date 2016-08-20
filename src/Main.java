adc0d3a08e8fde88ddf52bfd155a2d4093c28e84
octopusfly <381891675@qq.com>
2016-08-20 14:46:20 +0800

    -

--- src/Main.java
+++ src/Main.java
@@ -1,57 +1,39 @@
-
-import static java.lang.Math.*;
-
 import java.util.Scanner;
 
-/**
- * This is the ACM problem solving program for hihoCoder 1237.
- * 
- * @version 2016-08-18
- * @author Zhang Yufei.
- */
 public class Main {
-	// input data.
-	private static double x, y;
-	private static double r;
 
-	/**
-	 * The main program.
-	 * 
-	 * @param args
-	 *            The command line parameters.
-	 */
+	private static int[] list = new int[105000];
+	private static int[] prime = new int[1000];
+
+	private static int T;
+	private static int k;
+
+	public static void getPrime() {
+    	list[0] = list[1] = 1;
+    	for(int i = 0; i < 105000; i++) {
+    		list[i] = 0;
+    	}
+    	
+    	list[0] = list[1] = 1;
+    	
+    	for(int i = 2; i < 105000; i++) {
+    		if(list[i] == 0) {
+    			for(int j = 2; i * j < 105000; j++) {
+    				list[i * j] = 1
+    			}
+    		}
+    	}
+    }
+
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+		T = scan.nextInt();
 
-		x = scan.nextDouble();
-		y = scan.nextDouble();
-		r = scan.nextDouble();
-
-		double max = -1;
-		int max_x = -1, max_y = -1;
-		double eps = 1e-6;
-
-		for (int i = (int) floor(x + r); i >= (int) ceil(x - r); i--) {
-			int top = (int) floor(y + sqrt(pow(r, 2) - pow(i - x, 2)));
-			int bottom = (int) ceil(y - sqrt(pow(r, 2) - pow(i - x, 2)));
-
-			double dist_top = pow(i - x, 2) + pow(top - y, 2);
-			double dist_bottom = pow(i - x, 2) + pow(bottom - y, 2);
-
-			if (max - dist_top < -eps) {
-				max = dist_top;
-				max_x = i;
-				max_y = top;
-			}
-
-			if (max - dist_bottom < -eps) {
-				max = dist_bottom;
-				max_x = i;
-				max_y = bottom;
-			}
+		for (int i = 0; i < T; i++) {
+			k = scan.nextInt();
+			long result = prime[k - 1] * prime[k - 1] - k;
+			System.out.println(result);
 		}
-
-		System.out.println(max_x + " " + max_y);
 
 		scan.close();
 	}
