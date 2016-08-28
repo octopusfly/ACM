e087378858513626af46270c700eedb02618d127
octopusfly <381891675@qq.com>
2016-08-28 20:36:23 +0800

    -

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.util.Map.Entry;
 import java.util.Scanner;
 
 /**
@@ -11,15 +13,20 @@ public class Main {
 	// Input data.
 	private static int N, M;
 
-	// The graph.
-	private static Vertex[] graph;
-	private static Edge[][] matrix;
+	/*
+	 * Using the hash map to record the graph. The element is the edge of graph,
+	 * the key is the destination vertex and value is the length of edge.
+	 */
+	private static HashMap<Integer, Integer>[] graph;
 
-	// All the edges.
-	private static Edge[] edges;
+	/*
+	 * Record the level and the number of vertexes in the sub-tree.
+	 */
+	private static long[] level;
+	private static long[] node;
 
 	// The THD value.
-	private static int THD;
+	private static long THD;
 
 	/**
 	 * The main program.
@@ -28,53 +35,45 @@ public class Main {
 	 *            The command line parameters.
 	 *
 	 */
+	@SuppressWarnings("unchecked")
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 
 		N = scan.nextInt();
 		M = scan.nextInt();
 
-		graph = new Vertex[N];
-		edges = new Edge[N - 1];
-		matrix = new Edge[N][N];
+		graph = new HashMap[N];
+		level = new long[N];
+		node = new long[N];
 
 		/*
 		 * Store the graph using adjacent list.
 		 */
 		for (int i = 0; i < N; i++) {
-			graph[i] = new Vertex(N - 1);
+			graph[i] = new HashMap<Integer, Integer>();
+			level[i] = 0;
+			node[i] = 0;
 		}
 
 		for (int i = 0; i < N - 1; i++) {
-			edges[i] = new Edge();
 			int u, v, k;
 			u = scan.nextInt();
 			v = scan.nextInt();
 			k = scan.nextInt();
-
+			
 			u--;
 			v--;
-
-			edges[i].length = k;
-
-			edges[i].dst1 = u;
-			edges[i].dst2 = v;
-
-			graph[u].add(edges[i]);
-			graph[v].add(edges[i]);
-
-			matrix[u][v] = matrix[v][u] = edges[i];
+			
+			graph[u].put(v, k);
+			graph[v].put(u, k);
 		}
 
 		/*
 		 * Compute the weight of every edge and get the THD value.
 		 */
-		getWeight(graph[0]);
-
 		THD = 0;
-		for (int i = 0; i < N - 1; i++) {
-			THD += edges[i].length * edges[i].weight;
-		}
+		level[0] = 1;
+		getWeight(0);
 
 		/*
 		 * Get the operation and output the result.
@@ -90,10 +89,14 @@ public class Main {
 
 				u--;
 				v--;
-
-				THD -= matrix[u][v].length * matrix[u][v].weight;
-				matrix[u][v].length = k;
-				THD += matrix[u][v].length * matrix[u][v].weight;
+				
+				int child = level[u] < level[v] ? v : u;
+				THD -= node[child] * (N - node[child]) * graph[u].get(v);
+				graph[u].remove(v);
+				graph[v].remove(u);
+				graph[u].put(v, k);
+				graph[v].put(u, k);
+				THD += node[child] * (N - node[child]) * graph[u].get(v);
 			} else if (operation.equals("QUERY")) {
 				System.out.println(THD);
 			}
@@ -106,85 +109,19 @@ public class Main {
 	 * This function set the weight of the all edges using dfs.
 	 * 
 	 * @param cur
-	 *            The current vertex to operation.
+	 *            The current index of vertex to operation.
 	 * @return The number of vertexes in the next iterator.
 	 */
-	private static int getWeight(Vertex cur) {
-		cur.visited = true;
-		int count = 1;
-		for (int i = 0; i < cur.count; i++) {
-			Vertex dst = null;
-			if (cur != graph[cur.edges[i].dst1]) {
-				dst = graph[cur.edges[i].dst1];
-			} else {
-				dst = graph[cur.edges[i].dst2];
-			}
-
-			if (!dst.visited) {
-				int cnt = getWeight(dst);
-				cur.edges[i].weight = cnt * (N - cnt);
-				count += cnt;
+	private static void getWeight(int cur) {
+		node[cur] = 1;
+		for (Entry<Integer, Integer> e : graph[cur].entrySet()) {
+			int index = e.getKey();
+			if (level[index] == 0) {
+				level[index] = level[cur] + 1;
+				getWeight(index);
+				THD += e.getValue() * node[index] * (N - node[index]);
+				node[cur] += node[index];
 			}
 		}
-
-		return count;
-	}
-
-}
-
-/**
- * Define the class to record the edges in graph.
- * 
- * @author Zhang Yufei.
- */
-class Edge {
-	/**
-	 * The destination of the edge.
-	 */
-	int dst1, dst2;
-
-	/**
-	 * The weight of this edge.
-	 */
-	int weight;
-
-	/**
-	 * The length of this edge.
-	 */
-	int length;
-
-	Edge() {
-		dst1 = dst2 = -1;
-		weight = -1;
-		length = -1;
-	}
-}
-
-/**
- * Define class to record the vertexes in graph.
- * 
- * @author Zhang Yufei.
- *
- */
-class Vertex {
-	/**
-	 * The edges whose destination is this vertex.
-	 */
-	Edge[] edges;
-	int count;
-
-	/**
-	 * Mark if the vertex has been visited.
-	 */
-	boolean visited;
-
-	Vertex(int size) {
-		edges = new Edge[size];
-		count = 0;
-		visited = false;
-	}
-	
-	void add(Edge e) {
-		edges[count++] = e;
 	}
 }
