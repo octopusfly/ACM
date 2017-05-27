00e6637fd31b75c36fda10e954677231f93eccc2
octopusfly <381891675@qq.com>
2017-05-27 21:13:26 +0800

    hiho week 152

--- src/hihocoder/HihoCoder1305_2.java
+++ src/hihocoder/HihoCoder1305_2.java
@@ -1,6 +1,7 @@
+package hihocoder;
+
 import java.util.Arrays;
 import java.util.Scanner;
-
 
 /**
  * This is the ACM problem solving program for hihoCoder 1305.
@@ -8,43 +9,46 @@ import java.util.Scanner;
  * @version 2017-05-27
  * @author Zhang Yufei.
  */
-public class Main {
+public class HihoCoder1305_2 {
 	/**
 	 * Range A and range B.
 	 */
 	private static enum Range {
 		RA, RB;
 	}
-	
+
 	/**
 	 * Start or end edge of the given value.
 	 */
 	private static enum Type {
 		START, END;
 	}
-	
-	private static class Edge implements Comparable<Edge>{
+
+	/**
+	 * Record the edge value of each range.
+	 */
+	private static class Edge implements Comparable<Edge> {
 		Range range;
 		Type type;
 		int value;
-		
+
 		@Override
 		public int compareTo(Edge o) {
 			// TODO Auto-generated method stub
 			return value - o.value;
 		}
 	}
-	
+
 	/**
 	 * Input data.
 	 */
 	private static int N, M;
-	
+
 	/**
 	 * Range data list.
 	 */
 	private static Edge[] edges;
-	
+
 	/**
 	 * The main program.
 	 * 
@@ -53,55 +57,56 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		
+
 		N = scan.nextInt();
 		M = scan.nextInt();
-		
+
 		edges = new Edge[2 * (N + M)];
-		for(int i = 0; i < 2 * (N + M); i++) {
+		for (int i = 0; i < 2 * (N + M); i++) {
 			edges[i] = new Edge();
 			edges[i].value = scan.nextInt();
-			
-			if(i % 2 == 0) {
+
+			if (i % 2 == 0) {
 				edges[i].type = Type.START;
 			} else {
 				edges[i].type = Type.END;
 			}
-			
-			if(i >= 2 * N) {
+
+			if (i >= 2 * N) {
 				edges[i].range = Range.RB;
 			} else {
 				edges[i].range = Range.RA;
 			}
 		}
-		
+
 		scan.close();
-		
+
 		Arrays.sort(edges);
-		
+
 		int cntA = 0;
 		int cntB = 0;
 		int result = 0;
-		for(int i = 0; i < 2 * (N + M) - 1; i++) {
-			if(edges[i].type == Type.START) {
-				if(edges[i].range == Range.RA) {
+		for (int i = 0; i < 2 * (N + M) - 1; i++) {
+			if (edges[i].type == Type.START) {
+				if (edges[i].range == Range.RA) {
 					cntA++;
-				} else if(edges[i].range == Range.RB) {
+				} else if (edges[i].range == Range.RB) {
 					cntB++;
 				}
-			} else if(edges[i].type == Type.END) {
-				if(edges[i].range == Range.RA) {
+			} else if (edges[i].type == Type.END) {
+				if (edges[i].range == Range.RA) {
 					cntA--;
-				} else if(edges[i].range == Range.RB) {
+				} else if (edges[i].range == Range.RB) {
 					cntB--;
 				}
 			}
-			
-			if(cntA != 0 && cntB == 0) {
-				result += edges[i + 1].value - edges[i].value;
+
+			if (cntA != 0 && cntB == 0) {
+				result += edges[i + 1].value
+						- edges[i].value;
 			}
 		}
-		
+
 		System.out.println(result);
 	}
 }
